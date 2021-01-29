c29c805d41bb9e3ae8dba79e927653e9fa906903
Jennifer LaRue <jennifer@photoshelter.com>
2021-01-29 11:06:06 -0500

    meets expectation tests completed

--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@ For assistance:
 
 
 /*
-Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
@@ -28,6 +27,11 @@ function showPage(list, page) {
    for(let i = 0; i < list.length; i++) {
 
       if (i >= startIndex && i < endIndex) {
+
+         /*
+            variables below are student's information provided by the 
+            list parameter 
+         */ 
          
          let firstName = list[i].name.first;
          let lastName = list[i].name.last;
@@ -37,7 +41,10 @@ function showPage(list, page) {
          let date = list[i].registered.date;
          let image = list[i].picture.medium;
 
-         
+         /*
+            newHTML variable provides individual student info that will
+            be shown on the page
+         */
          let newHtml = `
             <li class="student-item cf">
                <div class="student-details">
@@ -52,162 +59,54 @@ function showPage(list, page) {
          `;
          
          studentList.insertAdjacentHTML('beforeend', newHtml);  
-
-      
-         // let li = document.createElement('li');
-         // //add class
-         // li.className ="student-item cf";
-         // //Add list
-         // studentList.appendChild(li);
-         // //find list
-         // const fullList = document.getElementsByClassName("student-item cf")[0];
-            
-         //    let studentDiv = document.createElement('div');
-         //    //add class
-         //    studentDiv.className = "student-details";
-         //    //Add div
-         //    fullList.appendChild(studentDiv);
-         //    //find new div items
-         //    const studentDetails = document.getElementsByClassName('student-details')[0];
-               
-         //       let imageFile = document.createElement('img');
-         //          //add class
-         //          imageFile.className = "avatar";
-         //          //add source
-         //          imageFile.src = image;
-         //          //add ALT 
-         //          imageFile.alt = "Profile Picture";
-         //       //addImage
-         //       studentDetails.appendChild(imageFile);
-         //       //find image
-         //       const studentImage = document.getElementsByClassName('avatar')[0];
-
-         //       let h3Name = document.createElement('h3');
-         //          //add text content
-         //          h3Name.textContent = fullName;
-         //       //addH3
-         //       studentDetails.insertAdjacentElement('beforeend', h3Name);
-         //       //find h3 elements
-         //       const studentName = document.getElementsByTagName('h3')[0];
-
-         //       let spanEmail = document.createElement('span');
-         //          //add class
-         //          spanEmail.className = "email";
-         //          //add textContent
-         //          spanEmail.textContent = email;
-         //          //add SPAN
-         //          studentDetails.insertAdjacentElement('beforeend', spanEmail);
-         //          //const studentEmail = document.getElementsByClassName('email');
-         
-         //    //div
-         //    let joinedDiv = document.createElement('div');
-         //       //add class
-         //          joinedDiv.className = "joined-details";
-         //    //add div2
-         //    studentDetails.insertAdjacentElement('afterend', joinedDiv);
-         //    //find div2
-         //    const joinedDetails = document.getElementsByClassName('joined-details')[0];
-
-         //    //span
-         //       let dateDetails = document.createElement('span');
-         //       //add class
-         //          dateDetails.className = "date";
-         //       //add text content
-         //          dateDetails.textContent = `Joined ${date}`;
-         //    //addSpan2
-         //    joinedDetails.appendChild(dateDetails);
-
-         //    // const test = studentList.innerHTML;
-         //    // for(let i = 0; i < studentList.length; i++) {
-         //    //    studentList[i].insertAdjacentElement('beforeend', test);
-         //    // }
-
-
-         //    studentList.innerHTML; 
-         //    console.log(studentList);
       }
    }
 }
-showPage(data, 2);
-         
-         
-         // //Create DOM & add DOM items
-         // let student = document.createElement('li');
-         // //add class
-         // student.className ="student-item cf";
-         // //Add list
-         // studentList.appendChild(student);
-            
-         //    let studentDiv =document.createElement('div');
-         //    //add class
-         //    studentDiv.classList.add = "student-details";
-         //    //Add div
-         //    student.appendChild(studentDetails);
-         //    //find new div items
-         //    const studentDetails = document.getElementsByClassName('student-details');
-               
-         //       let imageFile = document.createElement('img');
-         //          //add class
-         //          imageFile.classList.add = "avatar";
-         //          //add source
-         //          imageFile.src = image;
-         //          //add ALT 
-         //          imageFile.alt = "Profile Picture";
-         //       //addImage
-         //       studentDetails.appendChild(studentImage);
-         //       //find image
-         //       const studentImage = document.getElementsByClassName('avatar');
-
-         //       let h3Name = document.createElement('h3');
-         //          //add text content
-         //          h3Name.textContent = fullName;
-         //       //addH3
-         //       studentImage.insertAdjacentHTML('beforeend', fullName);
-         //       //find h3 elements
-         //       const studentName = document.querySelector('h3');
-
-         //          let spanEmail = document.createElement('SPAN');
-         //             //add class
-         //             spanEmail.class = "email";
-         //             //add textContent
-         //             spanEmail.textContent = email;
-         //          //add SPAN
-         //          studentName.insertAdjacentHTML('beforeend', spanEmail);
-         //          //const studentEmail = document.getElementsByClassName('email');
-         
-         // //div
-         // let joinedDiv = document.createElement('div');
-         //    //add class
-         //       joinedDiv.classList.add = "joined-details";
-         // //add div2
-         // studentDetails.insertAdjacentHTML('afterend', joinedDiv);
-         // //find div2
-         // const joinedDetails = document.getElementsByClassName('joined-details');
-
-         // //span
-         //    let dateJoined = document.createElement('SPAN');
-         //    //add class
-         //       dateJoined.classList.add = "date";
-         //    //add text content
-         //       dateJoined.textContent = date;
-         // //addSpan2
-         // joinedDetails.appendChild(dateJoined);    
-         
-         // studentList.innerHTML = html;
-         // return studentList
-
-
-
-
-
-
-
 
 /*
-Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+
+   let numOfPages = Math.ceil(list.length/9);
+
+   const linkList = document.querySelector('.link-list');
+
+   linkList.innerHTML = '';
+
+   for (let i = 1; i <= numOfPages; i++) {
+      let pageNumber = [i];
+      let button =
+         `<li>
+            <button type="button">${pageNumber}</button>
+         </li>`;
+      linkList.insertAdjacentHTML('beforeend', button);
+
+      let firstButton = document.querySelector('button');
+      firstButton.className = 'active';
+      }
+   
+      linkList.addEventListener('click', (e) => {
+         let clickEvent = e.target;
+         if(clickEvent.tagName == 'BUTTON') {
+            let activeButton = document.querySelector('.active');
+            activeButton.className = '';
+
+            clickEvent.className = 'active';
+            let clickEventContent = clickEvent.textContent;
+            console.log(clickEventContent);
+
+            showPage(list, clickEventContent);
+         }
+      });
+   }
+
+   
+   /**
+    * Call Functions
+    */ 
+   showPage(data, 1);
+   addPagination(data);
 
 
-// Call functions
